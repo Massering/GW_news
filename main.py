e890918575bca763600b71aa41f4e9d67f7e0dfc
Maksim Rudakov <massering@yandex.ru>
2026-05-12 04:44:29 +0300

    parser version + local LLM

--- main.py
+++ main.py
@@ -1,112 +1,63 @@
+import json
 import requests
-import schedule
 import time
 from bs4 import BeautifulSoup
 from telegram import Bot
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 from dotenv import load_dotenv
 
-# === LOAD ENV ===
+from parser import YandexArchiveParser, YEAR_URLS
+
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
-DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
-
-SOURCES = [
-    "https://example.com/ww2-article",
-]
-
 PROMPT_FILE = "prompt.txt"
 
-# === TELEGRAM ===
 bot = Bot(token=BOT_TOKEN)
 
 
-# === SOURCE PARSER ===
-def fetch_source_content(url):
-    try:
-        response = requests.get(url, timeout=10)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        paragraphs = soup.find_all("p")
-        text = "\n".join([p.get_text() for p in paragraphs])
-        return text[:5000]
-    except Exception as e:
-        print(f"Error fetching {url}: {e}")
-        return ""
-
-
-# === LOAD PROMPT ===
 def load_prompt():
     with open(PROMPT_FILE, "r", encoding="utf-8") as f:
         return f.read()
 
 
-# === LLM REQUEST ===
 def generate_news(text):
-    prompt_template = load_prompt()
-    full_prompt = prompt_template.replace("{{content}}", text)
+    full_prompt = load_prompt().replace("{{content}}", text)
 
-    url = "https://api.deepseek.com/v1/chat/completions"
-    headers = {
-        "Authorization": f"Bearer {DEEPSEEK_API_KEY}",
-        "Content-Type": "application/json",
-    }
+    response = requests.post("http://localhost:11434/api/generate", json={
+        "model": "deepseek-r1:7b",
+        "prompt": full_prompt,
+        "temperature": 0.7,
+        "stream": False
+    })
 
-    payload = {
-        "model": "deepseek-chat",
-        "messages": [
-            {"role": "user", "content": full_prompt}
-        ],
-        "temperature": 0.7
-    }
-
-    response = requests.post(url, headers=headers, json=payload)
-    data = response.json()
-
-    try:
-        return data["choices"][0]["message"]["content"]
-    except Exception:
-        print("LLM error:", data)
-        return None
+    return response.json()["response"]
 
 
-# === PUBLISH ===
 def publish(text):
-    if not text:
-        return
-    bot.send_message(chat_id=CHANNEL_ID, text=text)
+    if text:
+        bot.send_message(chat_id=CHANNEL_ID, text=text)
 
 
-# === JOB ===
 def job():
-    print("[", datetime.now(), "] Running job...")
+    target_date = datetime.now() - timedelta(days=365 * 84 + 23)
 
-    all_text = ""
-    for source in SOURCES:
-        content = fetch_source_content(source)
-        if content:
-            all_text += content + "\n\n"
+    parser = YandexArchiveParser()
+    data = parser.parse_issue_by_date(
+        YEAR_URLS[target_date.year],
+        target_date.strftime("%Y-%m-%d")
+    )
 
-    if not all_text:
-        print("No content fetched")
-        return
+    os.makedirs("Izvestia", exist_ok=True)
+    with open(f"Izvestia/izvestia_{target_date.strftime('%Y-%m-%d')}.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
 
+    all_text = '\n\n'.join([i["text"] for i in data["pages"]])
     news = generate_news(all_text)
     publish(news)
 
 
-# === SCHEDULER ===
-def run_scheduler():
-    schedule.every().day.at("10:00").do(job)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(30)
-
-
 if __name__ == "__main__":
-    run_scheduler()
+    job()
---
+++ parser.py
@@ -0,0 +1,248 @@
+import json
+import re
+import time
+from datetime import datetime, timedelta
+from urllib.parse import urljoin
+
+import cloudscraper
+from bs4 import BeautifulSoup
+
+YEAR_URLS = {
+    1941: "https://yandex.ru/archive/catalog/4b29b52e-1776-4d64-9eb6-4c4439cc7890/years/2061922a-998d-4b77-99ef-8f5d6ef0dafe",
+    1942: "https://yandex.ru/archive/catalog/4b29b52e-1776-4d64-9eb6-4c4439cc7890/years/a32ce5e2-8dd5-4f90-b614-95cb0f10d2ec",
+    1943: "https://yandex.ru/archive/catalog/4b29b52e-1776-4d64-9eb6-4c4439cc7890/years/0a2f650c-83d5-4ca3-a562-9ab2ae9029a9",
+    1944: "https://yandex.ru/archive/catalog/4b29b52e-1776-4d64-9eb6-4c4439cc7890/years/eda22010-2a45-43f9-a78e-55deedc9481b",
+    1945: "https://yandex.ru/archive/catalog/4b29b52e-1776-4d64-9eb6-4c4439cc7890/years/c0657fb1-6fee-41d4-885c-e56354665421"
+}
+
+BASE_URL = "https://yandex.ru"
+
+MONTHS = {
+    "января": 1,
+    "февраля": 2,
+    "марта": 3,
+    "апреля": 4,
+    "мая": 5,
+    "июня": 6,
+    "июля": 7,
+    "августа": 8,
+    "сентября": 9,
+    "октября": 10,
+    "ноября": 11,
+    "декабря": 12,
+}
+
+
+class YandexArchiveParser:
+
+    def __init__(self):
+
+        self.scraper = cloudscraper.create_scraper(
+            browser={
+                "browser": "chrome",
+                "platform": "windows",
+                "mobile": False
+            }
+        )
+
+        self.scraper.headers.update({
+            "Accept-Language": "ru-RU,ru;q=0.9,en;q=0.8",
+            "Referer": "https://yandex.ru/archive/",
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 YaBrowser/26.4.0.0 Safari/537.36"
+            ),
+        })
+
+    # =====================================================
+    # GET HTML
+    # =====================================================
+
+    def get_html(self, url: str) -> str:
+
+        print(f"[GET] {url}")
+
+        response = self.scraper.get(url, timeout=60)
+
+        if response.status_code != 200:
+            raise Exception(
+                f"HTTP {response.status_code} for {url}"
+            )
+
+        return response.text
+
+    def get_soup(self, url: str) -> BeautifulSoup:
+
+        html = self.get_html(url)
+
+        return BeautifulSoup(html, "lxml")
+
+    # =====================================================
+    # ПОИСК ВЫПУСКА ПО ДАТЕ
+    # =====================================================
+    def find_issue_by_date(self, year_url: str, target_date: str):
+        soup = self.get_soup(year_url)
+
+        target_dt = datetime.strptime(target_date, "%Y-%m-%d")
+
+        issue_cards = soup.select("div.Issues_ImageWrapper__XkXEo")
+
+        print(f"Найдено карточек: {len(issue_cards)}")
+
+        if len(issue_cards) == 0:
+           return None
+
+        for card in issue_cards:
+            date_el = card.select_one(
+                "div.Issues_ImageDate__8xfq9"
+            )
+
+            link_el = card.select_one("a[href]")
+
+            if not date_el or not link_el:
+                continue
+
+            raw_date = date_el.get_text(" ", strip=True)
+
+            match = re.search(
+                r"(\d+)\s+([а-я]+)\s+(\d{4})",
+                raw_date.lower()
+            )
+
+            if not match:
+                continue
+
+            day = int(match.group(1))
+            month_name = match.group(2)
+            year = int(match.group(3))
+
+            month = MONTHS.get(month_name)
+
+            if not month:
+                continue
+
+            parsed_date = datetime(year, month, day)
+
+            if parsed_date.date() == target_dt.date():
+                issue_url = urljoin(
+                    BASE_URL,
+                    link_el["href"]
+                )
+
+                return {
+                    "date": target_date,
+                    "issue_url": issue_url
+                }
+
+        return None
+
+    # =====================================================
+    # OCR ТЕКСТ
+    # =====================================================
+
+    def extract_ocr_text(self, soup: BeautifulSoup):
+
+        lines = soup.select(
+            "p.MarkupTextsViewer_RegionsListItem_Readonly__g3lYR"
+        )
+
+        result = []
+
+        for line in lines:
+
+            text = line.get_text(" ", strip=True)
+
+            if text:
+                result.append(text)
+
+        return "\n".join(result)
+
+    # =====================================================
+    # ПАРСИНГ СТРАНИЦЫ
+    # =====================================================
+
+    def parse_page(self, page_url: str):
+
+        soup = self.get_soup(page_url)
+
+        text = self.extract_ocr_text(soup)
+
+        return {
+            "url": page_url,
+            "text": text
+        }
+
+    # =====================================================
+    # ПОЛНЫЙ ПАРСИНГ ВЫПУСКА
+    # =====================================================
+
+    def parse_issue_by_date(self, year_url: str, target_date: str):
+        for i in range(1, 8):
+            issue = self.find_issue_by_date(
+                year_url + f"?pageNum={i}",
+                target_date
+            )
+            if issue:
+                break
+
+        else:
+            raise RuntimeError(f"Выпуск за {target_date} не найден")
+
+        issue_url = issue["issue_url"]
+
+        print(f"Найден выпуск:", issue_url)
+
+        page_urls = []
+
+        for page_num in range(1, 5):
+            page_urls.append(f"{issue_url}/{page_num}")
+
+        result = {
+            "date": target_date,
+            "issue_url": issue_url,
+            "pages": []
+        }
+
+        for i, page_url in enumerate(page_urls, start=1):
+            print(f"Парсим страницу {i}")
+
+            try:
+                page_data = self.parse_page(page_url)
+                result["pages"].append({
+                    "page": i,
+                    "url": page_url,
+                    "text": page_data["text"]
+                })
+
+                time.sleep(1)
+
+            except Exception as e:
+                print(f"Ошибка страницы {i}: {e}")
+
+        return result
+
+
+# =========================================================
+# MAIN
+# =========================================================
+
+if __name__ == "__main__":
+    parser = YandexArchiveParser()
+
+    target_date = datetime(1942, 5, 12)
+
+    data = parser.parse_issue_by_date(
+        YEAR_URLS[target_date.year],
+        target_date.strftime("%Y-%m-%d")
+    )
+
+    filename = f"izvestia_{target_date.strftime("%Y-%m-%d")}.json"
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(
+            data,
+            f,
+            ensure_ascii=False,
+            indent=2
+        )
+
+    print(f"\nСохранено в {filename}")
